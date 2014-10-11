649c9a69201feb4e7f9eea90630efe371a35705e
JOSIE <JOSIE@192.168.1.29>
2014-10-10 21:08:09 -0300

    Examen 10/10

---
+++ sessions/login/lib.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * Connect to DB
+ * @return stdClass
+ * @throws Exception
+ */
+function connect()
+{
+    $connect = mysqli_connect ("localhost", "root", "admin", "todolist");
+    if (!$connect) {
+        throw new Exception('Error trying to conect to DB: ' . mysqli_connect_error());
+    }
+    
+    return $connect;
+}
+
+/**
+ * Get User from DB
+ * @param type $name
+ * @return type
+ * @throws Exception
+ */
+function getUser($name)
+{
+
+    try {
+        $link = connect();
+        $result = mysqli_query($link, 'SELECT * FROM users WHERE name="' . $name . '"');
+        
+        return mysqli_fetch_assoc($result);
+    } catch (Exception $exc) {
+        logger($exc->getMessage());
+        throw $exc;
+    }
+}
+
+/**
+ * Get tasks from DB
+ * @param type $username
+ * @return type
+ */
+function getTasks($username)
+{
+    $user = getUser($username);
+    $link = connect();
+    $result = mysqli_query($link, 'SELECT * FROM tasks WHERE user_id=' . $user['user_id']);
+
+    return $result;
+}
+
+/**
+ * Add a Task to the DB
+ * @param type $username
+ * @param type $name
+ * @return type
+ */
+function addTask($username, $name)
+{
+    $user = getUser($username);
+    $link = connect();
+    $result = mysqli_query($link, 'INSERT INTO tasks (user_id, name) VALUES (' . $user['user_id'] . ', "' . $name . '")');
+
+    return $result;
+}
+
+/**
+ * Add user to the DB
+ * @param type $name
+ * @param type $pass
+ */
+function addUser($name, $pass)
+{
+    $link = connect();
+    $pass = md5($pass);
+    $result = mysqli_query($link, 'INSERT INTO users (name, password) VALUES ("' . $name . '", "' . $pass . '")');
+}
+
+/**
+ * 
+ * @param type $msg
+ */
+function logger($msg)
+{
+    file_put_contents('logs/log.txt', $msg . "\n", FILE_APPEND);
+}
+
+function deleteTask($id)
+{
+    $link = connect();
+    $resutl = mysqli_query($link, 'DELETE FROM tasks WHERE taskid=' . $id);
+    
+    return $resutl;
+}
+
+function blockUser($username)
+{
+    $user = getUser($username);
+    $link = connect();
+    $result = mysqli_query($link, 'UPDATE users SET blocked=1 WHERE user_id=' . $user['user_id']);
+}
+?>
--- sessions/login/login.php
+++ sessions/login/login.php
@@ -1,11 +1,35 @@
 <?php
-session_start();
 
-if(isset($_POST['name']) && isset($_POST['password']) && !empty($_POST['name'])) {
-  $_SESSION['name'] = $_POST['name'];
-  $_SESSION['pass'] = $_POST['password'];
-  Header('Location: todo.php');
-} else {
-  Header('Location: index.php?error=login');
+session_start();
+require_once 'lib.php';
+if (!isset($_SESSION['attempt'])){
+    $_SESSION['attempt']=1;
 }
+    
+if (isset($_POST['name']) && isset($_POST['password']) && !empty($_POST['name']) && $_SESSION['attempt']<4) {
+    try {
+        $user = getUser($_POST['name']);
+        if (!is_null($user) && $user['password'] == md5($_POST['password'])) {
+            $_SESSION['name'] = $_POST['name'];
+            $_SESSION['pass'] = $_POST['password'];
+            Header('Location: todo.php');
+        } else {
+            $_SESSION['attempt']+=1;
+            Header('Location: index.php?error=name or password incorrect');
+            }
+        } catch (Exception $exc) {
+        logger('Error trying to get user: '. $exc->getMessage());
+        
+        header('Location: error.php');
+    }
+
+} else {
+if ($_SESSION['attempt']>=4) {
+    Header('Location: index.php?error=You have tried to log in too many times.');
+    blockUser($_POST['name']);
+} else {
+    $_SESSION['attempt']+=1;
+    Header('Location: index.php?error=login');
+}}
+
 ?>
