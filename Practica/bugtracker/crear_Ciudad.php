08e71ad9dfb3d6c5b83fa17b4766ffef9ec3d62b
JOSIE <JOSIE@NBX>
2014-11-06 20:04:52 -0300

    Clase 06/11 - Load Provinces

--- Practica/bugtracker/crear_Ciudad.php
+++ Practica/bugtracker/crear_Ciudad.php
@@ -4,6 +4,7 @@ require_once 'bootstrap.php';
 $newCityName = $argv[1];
 
 $ciudad= new City();
+$ciudad->setName($newCiudadName);
 $provincia->setName($newProvinceName);
 
 $entityManager->persist($provincia);
